cb37f7578c0a1608b373e0c40f7743039e3b4734
Li-Heng Hsu <yesleon@me.com>
2021-03-08 03:14:42 +0800

    add file

---
+++ Sources/JSONKit/JSONCasting.swift
@@ -0,0 +1,20 @@
+//
+//  JSONCasting.swift
+//  
+//
+//  Created by Li-Heng Hsu on 2021/3/8.
+//
+
+import Foundation
+
+public protocol JSONCasting {
+    func cast<T: JSONData>(as type: T.Type) throws -> T
+}
+
+
+public extension JSONCasting where Self: JSONData {
+    
+    func cast<T: JSONData>(as type: T.Type = T.self) throws -> T {
+        return try castAsJSONData(self).cast(as: T.self)
+    }
+}
